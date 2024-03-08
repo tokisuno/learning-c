a7857fe7e2d145e6b22653df9112242b73f602ff
tokisuno <lucaswmei@gmail.com>
2024-03-07 19:32:39 -0500

    some chapter 2 programs

---
+++ 2/dweight.c
@@ -0,0 +1,21 @@
+// computes dimensional weight of a box
+// 12"x10"x8"
+
+#include <stdio.h>
+
+int main(void)
+{
+  int height, length, width, volume, weight;
+
+  height = 8;
+  length = 12;
+  width = 10;
+  volume = height * length * width;
+  weight = (volume + 165) / 166;
+
+  printf("Dimensions: %dx%dx%d\n", length, width, height);
+  printf("Volume (cubic inches): %d\n", volume);
+  printf("Dimensional weight (pounds): %d\n", weight);
+
+  return 0;
+}
---
+++ 2/pun.c
@@ -0,0 +1,11 @@
+// Name: pun.c             
+// Purpose: Prints bad pun 
+// Author: Lucas Cordero  
+
+#include <stdio.h>
+
+int main(void) // beginning of main program
+{
+  printf("to c, or not to c: that's the question.\n");
+  return 0;
+}
