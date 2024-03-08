579b32028d9c60fe843081b3dd9ea95db64182fd
tokisuno <lucaswmei@gmail.com>
2024-03-08 14:28:02 -0500

    question 8 complete; chapter 2 problems complete

---
+++ 2/projects/8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int months = 3;
+  double loan_amount = 20000.00f;
+  float interest_rate; 
+  double monthly_payment = 386.66f;
+  double result;
+  result = loan_amount;
+
+  printf("Input your monthly interest rate: ");
+  scanf("%f", &interest_rate);
+
+  interest_rate /= 100;
+  
+  while (months > 0)
+  {
+    result -= monthly_payment;
+    result += (result * (interest_rate / 12));
+    printf("Balance remaining after payment: %-.02f\n", result);
+    months--;
+  }
+  
+  return 0;
+}
