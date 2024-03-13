9b04f723485283d87b8b7565c6b1c5a23dd163a5
tokisuno <lucaswmei@gmail.com>
2024-03-13 17:14:45 -0400

    programming project

---
+++ 4/projects/03.c
@@ -0,0 +1,23 @@
+// * I had no idea how to write this so I found a solution online
+// * This just runs through howevery many numbers are in the loop
+
+
+#include <stdio.h>
+
+int main(void)
+{
+  int num; 
+  int rev_num = 0;
+  
+  printf("Enter two-digit number: ");
+  scanf("%d", &num);
+
+  while (num != 0)
+  {
+    rev_num = rev_num * 10  + (num % 10);
+    num = num / 10;
+  }
+  printf("Reversed number: %d\n", rev_num);
+
+  return 0;
+}
